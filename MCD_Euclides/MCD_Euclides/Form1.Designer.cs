2fc2d5f68d975350ee1305cc0b810151d91a8c0f
Fcomguerrero <fcomguerrero@hotmail.com>
2020-06-19 14:50:51 +0200

    MCM MCD Euclides

---
+++ MCD_Euclides/MCD_Euclides/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_mcd = new System.Windows.Forms.Label();
+            this.label_mcm = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Num_1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Num_2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label_mcd);
+            this.panel1.Controls.Add(this.label_mcm);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(7, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 71);
+            this.panel1.TabIndex = 5;
+            // 
+            // label_mcd
+            // 
+            this.label_mcd.AutoSize = true;
+            this.label_mcd.Location = new System.Drawing.Point(47, 55);
+            this.label_mcd.Name = "label_mcd";
+            this.label_mcd.Size = new System.Drawing.Size(10, 13);
+            this.label_mcd.TabIndex = 9;
+            this.label_mcd.Text = "-";
+            // 
+            // label_mcm
+            // 
+            this.label_mcm.AutoSize = true;
+            this.label_mcm.Location = new System.Drawing.Point(150, 54);
+            this.label_mcm.Name = "label_mcm";
+            this.label_mcm.Size = new System.Drawing.Size(10, 13);
+            this.label_mcm.TabIndex = 7;
+            this.label_mcm.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(112, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "MCM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MCD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Designed by Fco Guerrero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(241, 106);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "MCD - MCM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_mcm;
+        private System.Windows.Forms.Label label_mcd;
+    }
+}
+
---
+++ MCD_Euclides/MCD_Euclides/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void Mcd(int x, int y)
+        {
+            if (y == 0) {
+                label_mcd.Text = x.ToString();
+            }
+            else {
+                Mcd(y, x % y);
+            }
+        }
+        private void Mcm(int a, int b)
+        {
+            int x, z, y = 1, i = 2;
+
+            if (a == 0 || b == 0) { y = 0; label_mcm.Text = y.ToString(); }
+
+            if (a < b)
+            {
+                x = a;
+                z = b;
+            }
+            else
+            {
+                x = b;
+                z = a;
+            }
+            do {
+                if (x % i == 0)
+                {
+                    if (z % i == 0)
+                    {
+                        y = y * i;
+                        x = x / i; z = z / i; i = 2;
+                    }
+                    else i++;
+                }
+                else i++;
+            } while (i <= x);
+
+            y = y * z * x;
+
+            label_mcm.Text = y.ToString();
+        }
+        private bool ComprobarValores() {
+
+            bool rstado = false;
+            int number1 = 0;
+            int number2 = 0;
+            String numString1 = textBox1.Text.ToString();
+            String numString2 = textBox2.Text.ToString();
+            bool canConvert1 = Int32.TryParse(numString1, out number1);
+            bool canConvert2 = Int32.TryParse(numString2, out number2);            
+
+            if (canConvert1 == true && canConvert2 == true)
+                rstado = true;
+
+        return rstado;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool result = ComprobarValores();
+            if (result)        
+            {
+                Mcd(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                Mcm(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            }
+            else
+            {
+                MessageBox.Show("No es un NUMERO");                
+            }           
+        }       
+        private void button1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {            
+                if (e.KeyChar == Convert.ToChar(Keys.Enter))
+                {
+                    e.Handled = true;   //eliminar el sonido al presionar la tecla
+                    Mcd(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                    Mcm(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                }
+                else if (e.KeyChar == Convert.ToChar(Keys.Escape) || e.KeyChar == Convert.ToChar(Keys.Back))
+                {
+                    e.Handled = true;
+                    //limpiar textBox
+                    this.textBox1.Text = "";
+                    this.textBox2.Text = "";
+                    textBox1.Focus();
+                    textBox1.Select(textBox1.Text.Length, 0);
+                    this.label_mcd.Text = "-";
+                    this.label_mcm.Text = "-";
+                }
+                else
+                {
+                    //lo que haria en otro caso
+                }            
+        }    
+    }
+}
