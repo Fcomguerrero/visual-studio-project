7c96ee0ba2f4730a8e414057358b353ecca3c640
Fcomguerrero <fcomguerrero@hotmail.com>
2020-06-19 14:02:16 +0200

    OK
    Apagar monitor

---
+++ ApagarMonitor/ApagarMonitor/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ApagarMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::ApagarMonitor.Properties.Resources.off_luz;
+            this.button1.Location = new System.Drawing.Point(-1, -1);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 45);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(44, 42);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Location = new System.Drawing.Point(1310, 653);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.7D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "®Fran";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ApagarMonitor/ApagarMonitor/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices; //to DllImport
+
+namespace ApagarMonitor
+{
+    public partial class Form1 : Form
+    {
+        Boolean handled = false;
+        Boolean estaEncendido = true;
+        private const int MONITOR_ON = -1;
+        private const int MONITOR_STANBY = 1;
+        private const int MONITOR_OFF = 2;
+        public int WM_SYSCOMMAND = 0x0112;
+        public int SC_MONITORPOWER = 0xF170; //Using the system pre-defined MSDN constants that can be used by the SendMessage() function .
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+        //To call a DLL function from C#, you must provide this declaration .
+        //*****************************************************************************************
+        // Constantes para SetWindowsPos
+        //   Valores de wFlags
+        const int SWP_NOSIZE = 0x1;
+        const int SWP_NOMOVE = 0x2;
+        const int SWP_NOACTIVATE = 0x10;
+        const int wFlags = SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE;
+        //   Valores de hwndInsertAfter
+        const int HWND_TOPMOST = -1;
+        const int HWND_NOTOPMOST = -2;
+        [DllImport("user32.DLL")]
+        private extern static void SetWindowPos(
+               int hWnd, int hWndInsertAfter,
+               int X, int Y,
+               int cx, int cy,
+               int wFlags);
+
+        //******************************************************************************
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle.ToInt32(), HWND_TOPMOST, 0, 0, 0, 0, wFlags);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (handled)
+            {
+                handled = false;   //deshabilita para el boton Space
+                return;
+            }
+            if (estaEncendido)
+            {
+                estaEncendido = false;
+                SendMessage(this.Handle.ToInt32(), WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);                
+            }else{
+                // System.Threading.thread.sleep(28800000);
+                estaEncendido = true;
+                SendMessage(this.Handle.ToInt32(), WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_ON);
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) //cerrar Ventana con boton Middle Mouse
+            {
+                Application.ExitThread();
+            }
+        }
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space )   //deshabilita boton
+            {
+                handled = true;
+            }           
+        }        
+    }
+}
